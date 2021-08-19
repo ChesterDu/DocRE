debug no gradient bug

data.py
@@ -168,20 +168,23 @@ class graphDataset(torch.utils.data.Dataset):
             entPairs = []
             for i,label_data in enumerate(doc['labels']):
                 pair = [entity2node[label_data['h']],entity2node[label_data['t']]]
-                labels.append(rel2id[label_data['r']])
-                entPairs.append(pair)
+                if pair not in entPairs:
+                    labels.append(rel2id[label_data['r']])
+                    entPairs.append(pair)
             
             entNum = len(doc['vertexSet'])
+            naPairs = []
             for i in range(entNum):
                 for j in range(entNum):
                     if i==j:
                         continue
                     pair = [entity2node[i],entity2node[j]]
                     if pair not in entPairs:
-                        entPairs.append(pair)
+                        naPairs.append(pair)
                         labels.append(0)
             
             labels += [-1] * (self.max_pair_num - len(labels))
+            entPairs += naPairs
             entPairs += [[0,0] for i in range(self.max_pair_num-len(entPairs))]
 
             self.samples[doc_id]['finalLabels'] = labels

main.py
@@ -4,7 +4,7 @@ from random import shuffle
 
 from config import parse_config
 from trainner import Trainner
-from model import finalModel,make_embed,debugModel
+from model import finalModel,debugModel
 from data import graphDataset, build_vocab, collate_fn
 from opt import OpenAIAdam
 from torch.utils.data import DataLoader
@@ -14,6 +14,7 @@ import torch
 
 
 config = parse_config()
+print(config)
 
 
 random.seed(config.seed)

model.py
@@ -137,7 +137,7 @@ class finalModel(nn.Module):
         self.node_type_emb_dim = config.node_attr_emb_dim
 
         # Activation Func
-        self.pred_activation = make_activation(config.pred_activatino)
+        self.pred_activation = make_activation(config.pred_activation)
 
         # Embedding Layer: Convert word id to word feature representation
         if config.embed_type=='bert-base':
@@ -179,9 +179,25 @@ class finalModel(nn.Module):
                 torch.nn.init.xavier_normal_(p)
             else:
                 torch.nn.init.normal_(p)
-        torch.nn.init.xavier_normal_(self.nerEmb)
-        torch.nn.init.xavier_normal_(self.attrEmb)
-        torch.nn.init.xavier_normal_(self.relEmb)
+
+        for p in self.nerEmb.parameters():
+            if p.dim() >= 2:
+                torch.nn.init.xavier_normal_(p)
+            else:
+                torch.nn.init.normal_(p)
+
+        for p in self.attrEmb.parameters():
+            if p.dim() >= 2:
+                torch.nn.init.xavier_normal_(p)
+            else:
+                torch.nn.init.normal_(p)
+
+        for p in self.relEmb.parameters():
+            if p.dim() >= 2:
+                torch.nn.init.xavier_normal_(p)
+            else:
+                torch.nn.init.normal_(p)
+
         for p in self.pred_fc.parameters():
             if p.dim() >= 2:
                 torch.nn.init.xavier_normal_(p)
@@ -222,17 +238,18 @@ class finalModel(nn.Module):
 
     
     def forward(self,batch_data):
-        device = self.embed.device
+        device = self.config.device
         token_id = batch_data['token_id'].to(device) # [bsz,doc_len]
         token_feature = self.embed(token_id) # [bsz,doc_len,token_emb_dim]
+        bsz = token_id.shape[0]
 
         batched_node_in_feature = None
         batched_edge_in_feature = None
         node_offset = 0
         ent_pair_pos = batch_data['ent_pair'].to(device) #[bsz,max_pair_num,2]
+        batch_data['graph'] = [batch_data['graph'][i].to(device) for i in range(bsz)]
         for i,g in enumerate(batch_data['graph']):    
-            g = g.to(device)
-
+          
             if self.config.node_span_pool_method == 'avg':
                 node_span_mask = g.ndata['span_mask'] # [node_num,doc_len]
                 node_span_feature = node_span_mask.unsqueeze(-1) * (token_feature[i].unsqueeze(0).expand(g.num_nodes(),-1,-1)) # [node_num, doc_len, feature_dim]
@@ -251,8 +268,8 @@ class finalModel(nn.Module):
 
             edge_in_feature = self.relEmb(g.edata['edge_id'])
             
-            batched_node_in_feature = torch.cat([batched_node_in_feature,node_in_feature],dim=0) if batched_node_in_feature is None else node_in_feature
-            batched_edge_in_feature = torch.cat([batched_edge_in_feature,edge_in_feature],dim=0) if batched_edge_in_feature is None else edge_in_feature
+            batched_node_in_feature = torch.cat([batched_node_in_feature,node_in_feature],dim=0) if batched_node_in_feature != None else node_in_feature
+            batched_edge_in_feature = torch.cat([batched_edge_in_feature,edge_in_feature],dim=0) if batched_edge_in_feature != None else edge_in_feature
 
             ent_pair_pos[i] += node_offset
             node_offset += g.num_nodes()
@@ -261,7 +278,7 @@ class finalModel(nn.Module):
         h_feature = node_out_feature[ent_pair_pos[:,:,0]]
         t_feature = node_out_feature[ent_pair_pos[:,:,1]]
 
-        out = self.pred_fc(torch.cat(h_feature,t_feature,torch.abs(h_feature-t_feature)), h_feature * t_feature)
+        out = self.pred_fc(torch.cat([h_feature,t_feature,torch.abs(h_feature-t_feature), h_feature * t_feature],dim=-1))
 
         return out
         

trainner.py
@@ -7,12 +7,14 @@ import fitlog
 import tqdm
 
 
-class Trainner():
+class Trainner(nn.Module):
     def __init__(self,config,model,optimizer,criterion):
+        super(Trainner,self).__init__()
+        
         self.log_pth = config.log_pth
         self.checkpoint_pth = config.checkpoint_pth
         
-        self.model = copy.deepcopy(model).to(config.device)
+        self.model = model.to(config.device)
         self.optimizer = optimizer
         self.criterion = criterion
 
@@ -43,6 +45,9 @@ class Trainner():
         loss = self.criterion(logits.reshape(-1,logits.shape[-1]),labels.reshape(-1))
         loss.backward()
 
+        for p in self.model.embed.parameters():
+            print(p.grad)
+
         return loss
 
     def train(self,train_loader,dev_loader):
@@ -51,9 +56,14 @@ class Trainner():
         bar.update(0)
         while(self.step_count < self.total_steps):
             self.model.train()
+            epoch_loss = 0
             for batch_data in train_loader:
-                logits = self.forward_step(batch_data)
-                loss = self.backward_step(logits,batch_data['label'].to(self.device))
+                logits = self.model(batch_data)
+                logits = logits.reshape(-1,logits.shape[-1])
+                labels = batch_data['label'].to(self.device).reshape(-1)
+                loss = self.criterion(logits,labels)
+                epoch_loss += loss.item()
+                loss.backward()
                 self.forward_count += 1
 
                 if (self.forward_count % self.num_acumulation) == 0:
@@ -67,16 +77,16 @@ class Trainner():
 
 
                     if (self.step_count % self.loss_print_freq) == 0:
-                        print("Step:{}/{} Loss:{}".format(self.step_count,self.total_steps,loss.item()))
+                        print("Step:{}/{} || Loss:{}".format(self.step_count,self.total_steps,epoch_loss/self.forward_count))
                     
                     # if (self.step_count % self.metric_check_freq) == 0:
             print('Evaluation Start......')
             test_loss, test_total_acc, test_na_acc, test_non_na_acc = self.evaluate(dev_loader)
-            print("Eval Results Epoch: {} Step:{}/{} Loss:{} Acc: {} NA Acc: {} Non NA Acc: {}".format(self.epoch_count,self.step_count,self.total_steps,test_loss,test_total_acc, test_na_acc, test_non_na_acc))
-            fitlog.add_metric({"dev":{"Acc":test_total_acc}}, epoch=self.epoch_count)
-            fitlog.add_metric({"dev":{"NA Acc":test_na_acc}}, epoch=self.epoch_count)
-            fitlog.add_metric({"dev":{"Non NA Acc":test_non_na_acc}}, epoch=self.epoch_count)
-            fitlog.add_metric({"dev":{"Loss":test_loss}},epoch=self.epoch_count)
+            print("Eval Results Epoch: {} || Step:{}/{} || Loss:{} || Acc: {} || NA Acc: {} || Non NA Acc: {}".format(self.epoch_count,self.step_count,self.total_steps,test_loss,test_total_acc, test_na_acc, test_non_na_acc))
+            fitlog.add_metric({"dev":{"Acc":test_total_acc}}, step=self.step_count,epoch=self.epoch_count)
+            fitlog.add_metric({"dev":{"NA Acc":test_na_acc}}, step=self.step_count,epoch=self.epoch_count)
+            fitlog.add_metric({"dev":{"Non NA Acc":test_non_na_acc}}, step=self.step_count,epoch=self.epoch_count)
+            fitlog.add_metric({"dev":{"Loss":test_loss}},step=self.step_count,epoch=self.epoch_count)
             self.epoch_count += 1
     
 
@@ -89,21 +99,21 @@ class Trainner():
             total_na_pred = 0
             na_correct_pred = 0
             for batch_data in test_loader:
-                logits = self.forward_step(batch_data)
+                logits = self.model(batch_data)
                 logits = logits.reshape(-1,logits.shape[-1])
                 labels = batch_data['label'].to(self.device).reshape(-1)
                 loss = self.criterion(logits,labels)
                 test_loss.append(loss.item())
 
                 prediction = torch.argmax(logits,dim=1)
-                indices = (labels != -1).nonzero()
+                indices = (labels != -1).nonzero().squeeze(-1)
                 labels = labels[indices]
                 prediction = prediction[indices]
 
                 total_pred += prediction.shape[0]
                 correct_pred += torch.sum(prediction == labels).item()
 
-                na_indices = (labels == 0).nonzero()
+                na_indices = (labels == 0).nonzero().squeeze(-1)
                 na_pred = prediction[na_indices]
                 
                 na_correct_pred += torch.sum(na_pred == 0).item()
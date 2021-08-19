2a988840f0e675b6412311753b5a8a5e598fdac5
ChesterDu <yangkai.17@intl.zju.edu.cn>
2021-08-19 16:58:39 +0800

    fix config

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import random
 from random import shuffle
 
+from config import parse_config
 from trainner import Trainner
 from model import finalModel,make_embed,debugModel
 from data import graphDataset, build_vocab, collate_fn
@@ -10,38 +11,13 @@ from torch.utils.data import DataLoader
 import torch.backends.cudnn as cudnn
 import torch
 
-## Argument Parser
-parser = argparse.ArgumentParser()
-parser.add_argument('--seed',default=20,type=int)
-parser.add_argument('--train_batch_size',default=4,type=int)
-parser.add_argument('--num_acumulation',default=1,type=int)
-parser.add_argument('--eval_batch_size',default=8,type=int)
-parser.add_argument('--max_token_len',default=400,type=int)
-parser.add_argument('--embed_type',default='bert-base',type=str)
-parser.add_argument('--embed_pth',default='../pretrained_embed/bert-base-uncased',type=str)
-parser.add_argument('--node_dim',default=300,type=int)
-parser.add_argument('--node_out_dim',default=768,type=int)
-parser.add_argument('--edge_in_dim',default=768,type=int)
-parser.add_argument('--edge_dim',default=300,type=int)
-parser.add_argument('--M',default=3,type=int)
-parser.add_argument('--K',default=3,type=int)
-parser.add_argument('--L',default=2,type=int)
-parser.add_argument('--lr',default=1e-4,type=float)
-parser.add_argument('--clip',default=1.0,type=float)
-parser.add_argument('--total_steps',default=10000,type=int)
-parser.add_argument('--metric_check_freq',default=100,type=int)
-parser.add_argument('--loss_print_freq',default=10,type=int)
-parser.add_argument('--device',default=0,type=int)
-
-parser.add_argument('--log_pth',default='../logs/',type=str)
-parser.add_argument('--checkpoint_pth',default='../checkpoints/',type=str)
-
-args = parser.parse_args()
 
 
+config = parse_config()
 
-random.seed(args.seed)
-torch.manual_seed(args.seed)
+
+random.seed(config.seed)
+torch.manual_seed(config.seed)
 cudnn.deterministic = True
 
 ## Make dataloader
@@ -50,44 +26,34 @@ test_data_pth = "../DocRED/data/test_amr_annotated_full.json"
 dev_data_pth = "../DocRED/data/dev_amr_annotated_full.json"
 vocab = build_vocab(train_data_pth,test_data_pth,dev_data_pth)
 
-train_dataset = graphDataset(train_data_pth,vocab,seed=args.seed,max_token_len=args.max_token_len,ignore_label_idx=-1,split='train')
-# test_dataset = graphDataset(test_data_pth,vocab,seed=args.seed,max_token_len=args.max_token_len,ignore_label_idx=-1,split='test')
-dev_dataset = graphDataset(dev_data_pth,vocab,seed=args.seed,max_token_len=args.max_token_len,ignore_label_idx=-1,split='dev')
+train_dataset = graphDataset(train_data_pth,vocab,seed=config.seed,max_token_len=config.max_token_len,ignore_label_idx=-1,split='train')
+# test_dataset = graphDataset(test_data_pth,vocab,seed=config.seed,max_token_len=config.max_token_len,ignore_label_idx=-1,split='test')
+dev_dataset = graphDataset(dev_data_pth,vocab,seed=config.seed,max_token_len=config.max_token_len,ignore_label_idx=-1,split='dev')
 
-train_loader = DataLoader(train_dataset, num_workers=2, batch_size=args.train_batch_size, shuffle=True, collate_fn = collate_fn)
-# test_loader = DataLoader(test_dataset, num_workers=2, batch_size=args.eval_batch_size, shuffle=True, collate_fn= collate_fn)
-dev_loader = DataLoader(dev_dataset, num_workers=2, batch_size=args.train_batch_size, shuffle=True, collate_fn= collate_fn)
+train_loader = DataLoader(train_dataset, num_workers=2, batch_size=config.train_batch_size, shuffle=True, collate_fn = collate_fn)
+# test_loader = DataLoader(test_dataset, num_workers=2, batch_size=config.eval_batch_size, shuffle=True, collate_fn= collate_fn)
+dev_loader = DataLoader(dev_dataset, num_workers=2, batch_size=config.train_batch_size, shuffle=True, collate_fn= collate_fn)
 
 ## Make model
-embed_layer,node_in_dim = make_embed(vocab,args.embed_type,args.embed_pth)
-node_dim = args.node_dim
-node_out_dim = args.node_out_dim
-edge_in_dim = args.edge_in_dim
-edge_dim = args.edge_dim
-M = args.M
-K = args.K
-L = args.L
-device = args.device
-
-model = finalModel(embed_layer,node_in_dim,node_dim,node_out_dim,edge_in_dim,edge_dim,M,K,L)
+model = finalModel(vocab,config)
 # model = debugModel(embed_layer,node_in_dim,node_dim,node_out_dim,edge_in_dim,edge_dim,M,K,L).to(device)
 
 ## Make Optimizer and Criterion
-# optimizer = torch.optim.Adam(model.parameters(),lr=args.lr)
+# optimizer = torch.optim.Adam(model.parameters(),lr=config.lr)
 optimizer = OpenAIAdam(model.parameters(),
-                                  lr=args.lr,
+                                  lr=config.lr,
                                   schedule='warmup_linear',
                                   warmup=0.002,
-                                  t_total=args.total_steps,
+                                  t_total=config.total_steps,
                                   b1=0.9,
                                   b2=0.999,
                                   e=1e-08,
                                   l2=0.01,
                                   vector_l2=True,
-                                  max_grad_norm=args.clip)
-# optimizer = torch.optim.SGD(model.parameters(),lr=args.lr)
+                                  max_grad_norm=config.clip)
+# optimizer = torch.optim.SGD(model.parameters(),lr=config.lr)
 criterion = torch.nn.CrossEntropyLoss(ignore_index=-1)
 
 ## Make Trainner
-trainner = Trainner(args,model,optimizer,criterion)
+trainner = Trainner(config,model,optimizer,criterion)
 trainner.train(train_loader,dev_loader)
